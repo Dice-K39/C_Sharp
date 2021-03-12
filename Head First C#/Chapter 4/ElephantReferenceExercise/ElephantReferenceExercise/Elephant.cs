9bc04b78b2daf992b4fba2ec774aac79cf63806a
Dice-K39 <daisukehashimoto39@gmail.com>
2021-03-11 23:12:30 -0500

    Exercise swapping reference of objects.

---
+++ Head First C#/Chapter 4/ElephantReferenceExercise/ElephantReferenceExercise/Elephant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ElephantReferenceExercise
+{
+    public class Elephant
+    {
+        public string Name;
+        public int EarSize;
+
+        public Elephant()
+        {
+        }
+
+        public void WhoAmI()
+        {
+            Console.WriteLine("My name is " + Name + '.');
+            Console.WriteLine("My ears are " + EarSize + " inches tall.");
+        }
+    }
+}
---
+++ Head First C#/Chapter 4/ElephantReferenceExercise/ElephantReferenceExercise/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ElephantReferenceExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elephant lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
+            Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+
+            while (true)
+            {
+                Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+
+                char choice = Console.ReadKey(true).KeyChar;
+
+                Console.WriteLine("You pressed " + choice);
+
+                if (choice == '1')
+                {
+                    Console.WriteLine("Calling lloyd.WhoAmI()");
+
+                    lloyd.WhoAmI();
+                }
+                else if (choice == '2')
+                {
+                    Console.WriteLine("Calling lucinda.WhoAmI()");
+
+                    lucinda.WhoAmI();
+                }
+                else if (choice == '3')
+                {
+                    Console.WriteLine("References have been swapped");
+
+                    Elephant temp = lloyd;
+
+                    lloyd = lucinda;
+                    lucinda = temp;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice.");
+                }
+
+                Console.WriteLine("Press Q to quit, any other key to continue");
+
+                char character = Console.ReadKey(true).KeyChar;
+
+                if ((character == 'Q') || (character == 'q'))
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
