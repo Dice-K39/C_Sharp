bdd188bfe990e55d3f5bb5d2d14e983365149241
Dice-K39 <daisukehashimoto39@gmail.com>
2021-04-03 00:41:46 -0400

    Example of where encapsulation is not the same as security. Private fields are not secure.

---
+++ Head First C#/Chapter 5/HasASecret/HasASecret/HasASecret.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace HasASecret
+{
+    public class HasASecret
+    {
+        // This class has a secret field. Does the private keyword make it secure?
+        private string secret = "xyzzy";
+
+        public HasASecret()
+        {
+        }
+    }
+}
